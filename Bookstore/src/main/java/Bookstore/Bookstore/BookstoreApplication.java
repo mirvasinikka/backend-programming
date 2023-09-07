ad5127ac7a83b82d91bbc66e0968d8f57189e90b
mirvasinikka <mirva_s@hotmail.com>
2023-09-07 21:09:25 +0300

    add solution Chapter-3

--- Bookstore/src/main/java/Bookstore/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/Bookstore/Bookstore/BookstoreApplication.java
@@ -1,13 +1,38 @@
 package Bookstore.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import Bookstore.Bookstore.domain.Book;
+import Bookstore.Bookstore.domain.BookRepository;
+
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("couple of books");
+			repository.save(new Book("testi", "hello", 2020, 2345, 23));
+			repository.save(new Book("Moon", "Smith", 1996, 1234567890, 57));
+
+			log.info("get all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
+
+	}
 }
---
+++ Bookstore/src/main/java/Bookstore/Bookstore/domain/BookRepository.java
@@ -0,0 +1,11 @@
+package Bookstore.Bookstore.domain;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+	
+	List<Book> findByTitle(String title);
+
+	    
+}
