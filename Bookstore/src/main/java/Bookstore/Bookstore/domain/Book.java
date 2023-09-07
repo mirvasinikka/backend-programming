5083be2aace1d8db0895c5aeb585ea7a7b1a19cc
mirvasinikka <mirva_s@hotmail.com>
2023-09-07 17:48:29 +0300

    Moved BookStore domain under BookStore.BookStore

---
+++ Bookstore/src/main/java/Bookstore/Bookstore/domain/Book.java
@@ -0,0 +1,45 @@
+package Bookstore.Bookstore.domain;
+
+public class Book {
+	
+					
+					private String title;
+					private String author;
+					private int publicationYear;
+					private int isbn;
+					private int price;
+					
+					
+					public String getTitle() {
+						return title;
+					}
+					public void setTitle(String title) {
+						this.title = title;
+					}
+					public String getAuthor() {
+						return author;
+					}
+					public void setAuthor(String author) {
+						this.author = author;
+					}
+					public int getPublicationYear() {
+						return publicationYear;
+					}
+					public void setPublicationYear(int publicationYear) {
+						this.publicationYear = publicationYear;
+					}
+					public int getIsbn() {
+						return isbn;
+					}
+					public void setIsbn(int isbn) {
+						this.isbn = isbn;
+					}
+					public int getPrice() {
+						return price;
+					}
+					public void setPrice(int price) {
+						this.price = price;
+					}
+
+				}
+
